bd5d5bf1b9ef30ed6a789df1ce80a8f6abbce8fa
Gonzalo Guzman <gonzaguz@ucm.es>
2016-11-28 13:53:36 +0100

    Ejercicio 1 hecho

--- js/main.js
+++ js/main.js
@@ -38,22 +38,42 @@ battle.on('start', function (data) {
 battle.on('turn', function (data) {
     console.log('TURN', data);
 
-    // TODO: render the characters
-    // TODO: highlight current character
-    // TODO: show battle actions form
+    // TODO 1: render the characters
+    var heroes = document.querySelector('.character-listH');
+    var monsters = document.querySelector('.character-listM')
+
+    var allHeroes = battle.characters.allFrom('heroes');
+    var allMonsters = battle.characters.allFrom('monsters');
+
+    heroes.innerHTML = "";
+    monsters.innerHTML = "";
+
+    for (var i in allHeroes){
+     heroes.innerHTML += `<li data-chara-id="${allHeroes[i].name}"
+     > ${allHeroes[i].name} (HP: <strong>${allHeroes[i].hp}</strong>/${allHeroes[i].maxHp}
+			, MP: <strong>${allHeroes[i].mp}</strong>/${allHeroes[i].maxMp})</li>`;
+    }
+
+    for(var j in allMonsters){
+      monsters.innerHTML += `<li data-chara-id="${allMonsters[j].name}"
+      > ${allMonsters[j].name} (HP: <strong>${allMonsters[j].hp}</strong>/${allMonsters[j].maxHp}
+ 			, MP: <strong>${allMonsters[j].mp}</strong>/${allMonsters[j].maxMp})</li>`;
+    }
+    // TODO 2: highlight current character
+    // TODO 3: show battle actions form
 });
 
 battle.on('info', function (data) {
     console.log('INFO', data);
 
-    // TODO: display turn info in the #battle-info panel
+    // TODO 7: display turn info in the #battle-info panel
 });
 
 battle.on('end', function (data) {
     console.log('END', data);
 
-    // TODO: re-render the parties so the death of the last character gets reflected
-    // TODO: display 'end of battle' message, showing who won
+    // TODO 8: re-render the parties so the death of the last character gets reflected
+    // TODO 9: display 'end of battle' message, showing who won
 });
 
 window.onload = function () {
@@ -65,15 +85,15 @@ window.onload = function () {
     actionForm.addEventListener('submit', function (evt) {
         evt.preventDefault();
 
-        // TODO: select the action chosen by the player
-        // TODO: hide this menu
-        // TODO: go to either select target menu, or to the select spell menu
+        // TODO 4: select the action chosen by the player
+        // TODO 4: hide this menu
+        // TODO 4: go to either select target menu, or to the select spell menu
     });
 
     targetForm.addEventListener('submit', function (evt) {
         evt.preventDefault();
-        // TODO: select the target chosen by the player
-        // TODO: hide this menu
+        // TODO 5: select the target chosen by the player
+        // TODO 5: hide this menu
     });
 
     targetForm.querySelector('.cancel')
@@ -86,9 +106,9 @@ window.onload = function () {
 
     spellForm.addEventListener('submit', function (evt) {
         evt.preventDefault();
-        // TODO: select the spell chosen by the player
-        // TODO: hide this menu
-        // TODO: go to select target menu
+        // TODO 6: select the spell chosen by the player
+        // TODO 6: hide this menu
+        // TODO 6: go to select target menu
     });
 
     spellForm.querySelector('.cancel')
